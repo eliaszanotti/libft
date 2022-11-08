c98e54cc93f65fd7efa413885bb642182d6a2f99
Elias Zanotti <ezanotti@z2r8p4.42lyon.fr>
2022-11-08 15:17:43 +0100

    change memmove : nrm

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,15 +6,15 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 20:48:28 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/08 15:14:54 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/08 15:17:35 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <strings.h>
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t		i;
 	const char	*copy_src;
 	char		*copy_dest;
 	char		buffer[4096];
