48d3f6d75539044e2cb3199850e919836cba572f
Elias Zanotti <ezanotti@z2r8p4.42lyon.fr>
2022-11-08 17:47:43 +0100

    add calloc

---
+++ srcs/ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 17:06:42 by ezanotti          #+#    #+#             */
+/*   Updated: 2022/11/08 17:35:46 by ezanotti         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_calloc(size_t nelem, size_t elsize)
+{
+	size_t	size;
+	char	*rtr;
+
+	size = nelem * elsize;
+	rtr = malloc(size * sizeof(void));
+	while (size-- > 0)
+		rtr[size] = 0;
+	return (rtr);
+}
