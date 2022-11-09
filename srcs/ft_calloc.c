397f828e1e0833bda2a518aca15ed7ced631e843
Elias Zanotti <ezanotti@z1r2p3.42lyon.fr>
2022-11-09 22:18:33 +0100

    change calloc

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 17:06:42 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/09 10:09:43 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/09 22:18:00 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 void	*ft_calloc(size_t nelem, size_t elsize)
 {
 	size_t	size;
-	char	*rtr;
+	char	*new_s;
 
 	size = nelem * elsize;
-	rtr = malloc(size * sizeof(void));
-	while (size-- > 0)
-		rtr[size] = 0;
-	return (rtr);
+	if (size < nelem || size < elsize)
+		return (0);
+	new_s = malloc((size) * sizeof(void));
+	ft_bzero(new_s, size);
+	return (new_s);
 }
