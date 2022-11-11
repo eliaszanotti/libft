b5aa8ae7ef1baf82519c18ad999e4c1e258d1169
Elias Zanotti <ezanotti@z2r6p4.42lyon.fr>
2022-11-11 16:58:39 +0100

    add strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,47 +6,25 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 12:11:35 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/11 15:39:48 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/11 16:58:07 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isonset(char c, char const *set)
-{
-	while (*set)
-	{
-		if (*set == c)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
-	int		count;
-	int		i;
 	char	*new_s;
+	size_t	len;
 
-	len = ft_strlen(s1);
-	count = 0;
-	i = 0;
-	while (s1[i])
-	{
-		if (ft_isonset(s1[i], set))
-			count++;
-		i++;
-	}
-	new_s = malloc((len - count + 1) * sizeof(char));
-	i = 0;
-	while (*s1)
-	{
-		if (ft_isonset(*s1, set) == 0)
-			new_s[i++] = *s1;
+	
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
 		s1++;
-	}
-	new_s[i] = '\0';
+	len = ft_strlen(s1);
+	while (len && ft_strchr(set, s1[len]))
+		len--;
+	new_s = ft_substr(s1, 0, len + 1);
 	return (new_s);
 }
