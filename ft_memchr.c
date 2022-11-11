248a733b2932f533708c0c061461df188de07f74
Elias Zanotti <ezanotti@z2r6p4.42lyon.fr>
2022-11-11 16:38:07 +0100

    change memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:34:13 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/11 15:51:16 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/11 16:37:48 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*str;
+	size_t	i;
+	char	*str;
 
 	i = 0;
-	str = (unsigned char *)s;
+	str = (char *)s;
 	while (str[i] && i < n)
 	{
 		if ((unsigned char)str[i] == (unsigned char)c)
-			return ((char *)(str + i));
+			return ((char *)(s + i));
 		i++;
 	}
 	return (0);
