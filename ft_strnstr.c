83ac3a3bf745e361cfeb191007513d542b3ea100
Elias ZANOTTI <zanotti.elias@gmail.com>
2022-11-11 21:36:29 +0100

    change strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,18 +14,18 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t			i;
-	size_t			j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	if (!haystack || !needle)
-		return (ft_strdup(""));
+		return (0);
 	while (haystack[i] && i < len)
 	{
 		j = 0;
 		while (i + j < len && haystack[i + j] == needle[j] && needle[j])
 			j++;
-		if (needle[j] == '\0')
+		if (!needle[j])
 			return ((char *)haystack + i);
 		i++;
 	}
