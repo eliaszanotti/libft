589dc75777d6eb122fe838e70767b50dd61263b4
Elias Zanotti <ezanotti@z2r7p2.42lyon.fr>
2022-11-14 10:47:08 +0100

    change strnstr : add protection

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:07:19 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/11 17:47:33 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/14 10:46:54 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (!haystack || !needle)
-		return (0);
-	while (haystack[i] && i < len)
+	if (!needle || !*needle)
+		return ((char *)haystack);
+	while (i < len && haystack[i])
 	{
 		j = 0;
-		while (i + j < len && haystack[i + j] == needle[j] && needle[j])
+		while (haystack[i + j] == needle[j] && haystack[i + j] && i + j < len)
 			j++;
-		if (!needle[j])
-			return ((char *)haystack + i);
+		if (j == ft_strlen(needle))
+			return (((char *)haystack) + i);
 		i++;
 	}
 	return (0);
