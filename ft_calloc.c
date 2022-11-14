d0377d6a6f05524ffdae10a8725f43bc29c04901
Elias Zanotti <ezanotti@z2r7p2.42lyon.fr>
2022-11-14 11:07:21 +0100

    change calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,28 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 17:06:42 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/10 12:43:05 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/14 11:05:24 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nelem, size_t elsize)
+void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	size;
+	size_t	total;
 	char	*new_s;
 
-	size = nelem * elsize;
-	if (size < nelem || size < elsize)
-		return (NULL);
-	new_s = malloc((size) * sizeof(void));
+	total = count * size;
+	if (!count || !size)
+		return (malloc(0));
+	if (SIZE_MAX / size < count)
+		return (0);
+	new_s = malloc(total);
 	if (!new_s)
-		return (NULL);
-	ft_bzero(new_s, size);
+		return (0);
+	while (total > 0)
+		new_s[total--] = '\0';
+
+	//ft_bzero(new_s, total);
 	return (new_s);
 }
