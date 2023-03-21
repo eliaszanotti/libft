e466211900efe4cb549dc9fe4f5bb9c573dd85f0
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-03-21 14:45:39 +0100

    change file : fix putstr

--- put/ft_putstr_fd.c
+++ put/ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 18:54:40 by ezanotti          #+#    #+#             */
-/*   Updated: 2023/03/21 12:46:28 by elias            ###   ########.fr       */
+/*   Updated: 2023/03/21 14:45:00 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_putstr(char *s, int fd)
+int	ft_putstr_fd(char *s, int fd)
 {
 	int	total;
 
